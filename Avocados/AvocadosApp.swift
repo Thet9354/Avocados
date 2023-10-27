39fee6654eb7de6b171913f37cbc0d08b26601cf
PhoonThetPine <thetpine254@gmail.com>
2023-10-27 16:37:16 +0800

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Phoon Thet Pine on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
