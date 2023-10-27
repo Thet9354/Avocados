388b12c12ae4df47479f77d82e468b5f7b9f0d39
PhoonThetPine <thetpine254@gmail.com>
2023-10-27 23:11:52 +0800

    Avocados - V2

--- Avocados/Model/HeaderModel.swift
+++ Avocados/Model/HeaderModel.swift
@@ -5,4 +5,13 @@
 //  Created by Phoon Thet Pine on 27/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - HEADER MODEL
+
+struct Header: Identifiable {
+    var id = UUID()
+    var image: String
+    var headline: String
+    var subheadline: String
+}
